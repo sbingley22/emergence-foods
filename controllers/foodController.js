905df5364f3937208f0a91a3a4d7ef172fcde7db
Sean B <seanb4@hotmail.co.uk>
2023-12-11 12:25:24 +0000

    Finished food controller get create and delete

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,5 +1,7 @@
 const Category = require("../models/category")
 const Food = require("../models/food")
+const { body, validationResult  } = require("express-validator");
+
 const asyncHandler = require("express-async-handler")
 
 exports.food_list = asyncHandler(async (req, res, next) => {
@@ -32,19 +34,71 @@ exports.food_detail = asyncHandler(async (req, res, next) => {
 })
 
 exports.food_create_get = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented")
+    const categories = await Category.find().exec()
+
+    res.render("foodForm", {
+        title: "New food",
+        food: undefined,
+        categories: categories,
+        errors: [],
+    })
 })
 
-exports.food_create_post = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented")
-})
+exports.food_create_post = [
+    body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Name must be specified."),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        console.log(req.body.categories)
+
+        const food = new Food({
+            name: req.body.name,
+            items_left: req.body.items_left,
+            sell_by_date: req.body.sell_by_date,
+            category: req.body.categories,
+        })
+
+        if (!errors.isEmpty()) {
+            //Errors in form, rerender with sanitized values.
+            console.log("Errors in form")
+            const categories = await Category.find().exec()
+
+            res.render("foodForm", {
+                title: "New food",
+                food: food,
+                categories: categories,
+                errors: errors.array(),
+            })
+            return
+        } else {
+            //Save category to database
+            console.log("Saving new food to Database")
+            await food.save();
+            res.redirect(food.url)
+        }
+    })
+]
 
 exports.food_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented")
+    const food = await Food.findById(req.params.id).exec()
+
+    res.render("foodDelete", {
+        title: "Delete Food",
+        food: food,
+    })
 })
 
 exports.food_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented")
+    const food = await Food.findById(req.params.id).exec()
+
+    console.log("Deleting food")
+    await Food.findByIdAndDelete(req.body.foodid);
+    res.redirect("/store")
 })
 
 exports.food_update_get = asyncHandler(async (req, res, next) => {
